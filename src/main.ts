739448de89400f8a2df9b99fade157154ddc9d22
Kevin Soules <inceax64@gmail.com>
2016-07-22 06:26:19 +0100

    Use hashlocation instead of slashlocation.

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { HTTP_PROVIDERS } from '@angular/http';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
 import { APP_ROUTER_PROVIDERS } from './app/app.routes';
-
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent, [HTTP_PROVIDERS, APP_ROUTER_PROVIDERS]);
+bootstrap(AppComponent, [HTTP_PROVIDERS, APP_ROUTER_PROVIDERS, {provide: LocationStrategy, useClass: HashLocationStrategy}]);
 
