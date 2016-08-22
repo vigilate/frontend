1ccd8576fac075f641dffa4e3202c79b0e7ddf0d
Kevin Soules <inceax64@gmail.com>
2016-08-22 13:17:36 +0100

    Add tab for station on alerts

--- src/app/station.pipe.ts
+++ src/app/station.pipe.ts
@@ -3,11 +3,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'station'})
 export class StationPipe implements PipeTransform {
 
-    transform(prog_list: any, args): string {
-	let station_id = args;
-
-	return prog_list.filter(prog => {
-	    return station_id == "all" || prog.poste == station_id;
+    transform(list: any, arg_type, station_id): string {
+	if (arg_type == "prog")
+	    return list.filter(obj => {
+		return station_id == "all" || obj.poste == station_id;
+	    });
+	return list.filter(obj => {
+	    return station_id == "all" || obj.program_info.poste == station_id;
 	});
     }
 }
