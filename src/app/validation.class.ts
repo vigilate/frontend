18c192458bd4f87227f7c94f68f2adaf4eb219e5
Kevin Soules <inceax64@gmail.com>
2016-08-27 04:43:26 +0100

    Check the validity of the entered email. Fix #74

--- src/app/validation.class.ts
+++ src/app/validation.class.ts
@@ -28,3 +28,15 @@ export const TriggerValidator = (ctrl, toBeMatched) => {
 	return null;
     };
 };
+
+export class EmailValidator { 
+    static isValid(control: Control): ValidationResult {
+	if (control.value == "" || control.value == null)
+	    return null;
+	
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if (re.test(control.value))
+	    return null;
+	return {"isValidEmail": true};
+    }
+}
